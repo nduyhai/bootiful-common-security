3676666762ff458a699b5875b7294f5390a00da6
Hai Nguyen <haind.vng@gmail.com>
2022-01-08 20:28:53 +0700

    init project

---
+++ src/main/java/com/bootiful/common/security/BootifulCommonSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.bootiful.common.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootifulCommonSecurityApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BootifulCommonSecurityApplication.class, args);
+  }
+
+}
